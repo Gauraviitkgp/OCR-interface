021a0ef8fb6b209bb3ff4ea78af7f805b557da31
Gaurav <gauravs123456789@gmail.com>
2021-03-13 22:57:53 +0530

    V1-perfectly exectuing python code

---
+++ run.py
@@ -0,0 +1,84 @@
+import flask
+from flask import request, jsonify
+import base64 
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+import pytesseract
+import threading 
+import time
+
+class img_rqst():
+    def __init__(self,request,requestID=0):
+        self.request    = request
+        self.requestID  = requestID
+        self.data       = request.get_json(force=True)
+        self.img        = None
+        self.encoding   = request.charset #Encoding
+        self.tess_otpt  = None
+        self.dict_otpt  = {"text":None}
+
+        self.dict_rID   = {"task_id": str(requestID)}
+        # self.run_tesseract()
+
+    def run_tesseract(self):
+        time.sleep(20)
+        self.__decode_img__()
+        self.__apply_tess__()
+        
+
+    def __decode_img__(self):
+        encoded64_image = self.data['image_data']
+        image_64_decode = base64.decodebytes(encoded64_image.encode(encoding=self.encoding))
+        nparr           = np.frombuffer(image_64_decode, np.uint8)
+        self.img        = cv2.imdecode(nparr,0)
+
+    def __apply_tess__(self):
+        self.tess_otpt = pytesseract.image_to_string(self.img)
+        self.dict_otpt = {"text":self.tess_otpt}
+
+    def show(self,windowname="Input_Image",waitkey=1000):
+        cv2.imshow(windowname,self.img)
+        cv2.waitKey(waitkey)
+
+    def get_text(self):
+        print(self.tess_otpt)
+        return jsonify(self.dict_otpt)
+
+tasks   = {}
+threads = []
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+def start_tess(rqst):
+    rqst.run_tesseract()
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return "<h1>OCR engine </h1><p>This site is for recieving and OCR output for sample inputs.</p>"
+
+@app.route('/image-sync', methods=['GET','POST'])
+def recognize():
+    if request.method == 'POST':
+        rqst = img_rqst(request,len(tasks))
+        tasks[rqst.requestID] = rqst
+        A = threading.Thread(target=start_tess, args=(rqst,))
+        threads.append(A)
+        A.start()
+        return jsonify(rqst.dict_rID)
+    return "Request not processed.\n"
+
+@app.route('/image', methods=['GET','POST'])
+def check():
+    data       = request.get_json(force=True)
+    if int(data["task_id"]) not in tasks:
+        return "ERROR: The specified task id does not exist. Please check"
+    else:
+        return jsonify(tasks[int(data["task_id"])].dict_otpt)
+    
+app.run()
+# x = threading.Thread(target=recognize, args=())
+for thread in threads:
+    thread.join()
---
+++ run_post_cmd.py
@@ -0,0 +1,32 @@
+import cv2
+import os
+import base64
+import requests
+#remove-item alias:\curl
+
+
+def display_image(Img):
+    cv2.imshow('Image',Img)
+    cv2.waitKey(1000)
+
+def send_data(jpg_as_text):
+    url = "http://localhost:5000/image-sync"
+    data = {"image_data": "\""+str(jpg_as_text)[2:-1]+"\""}
+
+    res = requests.post(url, data=data)
+    print(res.text)
+
+imagename = 'test_img/img1.jpg'
+
+Img             = cv2.imread(imagename)
+# display_image(Img) 
+retval, buffer  = cv2.imencode(imagename[-4:], Img)
+jpg_as_text     = base64.b64encode(buffer)
+
+# send_data(jpg_as_text)
+
+# Get Curl command for the image
+J = "curl -XPOST \"http://localhost:5000/image-sync\" -d '{\\\"image_data\\\":\\\""+str(jpg_as_text)[2:-1]+"\\\"}'"
+print(J)
+
+# print("\\")
