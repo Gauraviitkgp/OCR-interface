4ef37b359a9c0959b2044fb5cce828a614c8f569
Gaurav <gauravs123456789@gmail.com>
2021-03-14 14:00:34 +0530

    Improved error handling, working code sender

--- run.py
+++ run.py
@@ -10,28 +10,44 @@ import time
 
 class img_rqst():
     def __init__(self,request,requestID=0):
+        self.error      = {"error":0,"message":""}
+
         self.request    = request
         self.requestID  = requestID
         self.data       = request.get_json(force=True)
+        if self.__check_for_errors__():
+            return
         self.img        = None
         self.encoding   = request.charset #Encoding
         self.tess_otpt  = None
         self.dict_otpt  = {"text":None}
 
         self.dict_rID   = {"task_id": str(requestID)}
+        
         # self.run_tesseract()
-
+        
     def run_tesseract(self):
         time.sleep(20)
-        self.__decode_img__()
+        if self.__decode_img__():
+            return
         self.__apply_tess__()
         
+    def __check_for_errors__(self):
+        if 'image_data' not in self.data:
+            self.error  = {"error":1,"message":"ERROR: Please Enter \"image_data\" data in column"}
+            return 1
+
 
     def __decode_img__(self):
         encoded64_image = self.data['image_data']
-        image_64_decode = base64.decodebytes(encoded64_image.encode(encoding=self.encoding))
+        try:
+            image_64_decode = base64.decodebytes(encoded64_image.encode(encoding=self.encoding))
+        except:
+            self.error  = {"error":1,"message":"ERROR: Incorrect Encoding Please Re-Check"}
+            return 1
         nparr           = np.frombuffer(image_64_decode, np.uint8)
         self.img        = cv2.imdecode(nparr,0)
+        return 0
 
     def __apply_tess__(self):
         self.tess_otpt = pytesseract.image_to_string(self.img)
@@ -63,6 +79,9 @@ def home():
 def recognize():
     if request.method == 'POST':
         rqst = img_rqst(request,len(tasks))
+        if rqst.error["error"]:
+            return rqst.error["message"]
+
         tasks[rqst.requestID] = rqst
         A = threading.Thread(target=start_tess, args=(rqst,))
         threads.append(A)
@@ -78,13 +97,16 @@ def check():
         task_id = int(data["task_id"])
     except ValueError:
         return "ERROR: Please enter an integer value in task_id column"
+    except KeyError:
+        return "ERROR: Please enter a column with name \"task_id\""
 
     if task_id not in tasks:
         return "ERROR: The specified task id does not exist. Please check"
     else:
+        if tasks[task_id].error["error"]:
+            return jsonify(tasks[task_id].error)
         return jsonify(tasks[task_id].dict_otpt)
     
 app.run()
-# x = threading.Thread(target=recognize, args=())
 for thread in threads:
     thread.join()
